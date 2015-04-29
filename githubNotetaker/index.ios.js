d38722cc959069b8125bd4f75345610e6546d145
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-04-29 16:16:10 -0400

    committing after testing/finishing

---
+++ githubNotetaker/index.ios.js
@@ -0,0 +1,33 @@
+var React = require('react-native');
+var Main = require('./App/Components/Main');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View,
+} = React;
+
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  },
+});
+
+class githubNotetaker extends React.Component{
+  render() {
+    return (
+      <NavigatorIOS
+      style={styles.container}
+        initialRoute={{
+          title: 'Github NoteTaker',
+          component: Main 
+        }} />
+    );
+  }
+};
+
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
